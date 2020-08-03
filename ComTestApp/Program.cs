96a7b3cd8d1cb33eb5100be1c64005dfc0ccee05
declineaberdeen <declineaberdeen@foxmail.com>
2020-08-03 13:59:21 +0800

    禁止多开

--- ComTestApp/Program.cs
+++ ComTestApp/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -31,7 +34,15 @@ namespace ComTestApp
             if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
             {
                 //如果是管理员，则直接运行
-                Application.Run(new FormMain());
+                Process process = RuningInstance();
+                if (process == null)
+                {
+                    Application.Run(new FormMain());
+                }
+                else
+                {
+                    HandleRunningInstance(process);
+                }
             }
             else
             {
@@ -81,6 +92,47 @@ namespace ComTestApp
                     fs.Write(byFont, 0, byFont.Length);
                 }
             }
+        }
+
+        ///<summary>
+        /// 该函数设置由不同线程产生的窗口的显示状态
+        /// </summary>
+        /// <param name="hWnd">窗口句柄</param>
+        /// <param name="cmdShow">指定窗口如何显示。查看允许值列表，请查阅ShowWlndow函数的说明部分</param>
+        /// <returns>如果函数原来可见，返回值为非零；如果函数原来被隐藏，返回值为零</returns>
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
+
+        /// <summary>
+        ///  该函数将创建指定窗口的线程设置到前台，并且激活该窗口。键盘输入转向该窗口，并为用户改各种可视的记号。
+        ///  系统给创建前台窗口的线程分配的权限稍高于其他线程。 
+        /// </summary>
+        /// <param name="hWnd">将被激活并被调入前台的窗口句柄</param>
+        /// <returns>如果窗口设入了前台，返回值为非零；如果窗口未被设入前台，返回值为零</returns>
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private const int SW_SHOWNOMAL = 5;
+        private static void HandleRunningInstance(Process instance)
+        {
+            ShowWindowAsync(instance.MainWindowHandle, SW_SHOWNOMAL);//显示
+            SetForegroundWindow(instance.MainWindowHandle);//设置到最前端
+        }
+        private static Process RuningInstance()
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            Process[] Processes = Process.GetProcessesByName(currentProcess.ProcessName);
+            foreach (Process process in Processes)
+            {
+                if (process.Id != currentProcess.Id)
+                {
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == currentProcess.MainModule.FileName)
+                    {
+                        return process;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
